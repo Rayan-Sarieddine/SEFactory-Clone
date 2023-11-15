7380b0dde6f8ec002edd0a2fd580e0edd5d14eba
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-11-15 14:04:49 +0200

    implemented hero

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+/*Hero functionalities */
+const navigation = document.querySelector("nav");
+const navigation_open_button = document.querySelector(".hero-menu_button");
+const navigation_close_button = document.querySelector(".button-hidden");
+const hero_section = document.querySelector(".hero-main");
+
+navigation_open_button.addEventListener("click", () => {
+  navigation.classList.remove("hidden");
+  hero_section.classList.add("blur");
+});
+navigation_close_button.addEventListener("click", () => {
+  navigation.classList.add("hidden");
+  hero_section.classList.remove("blur");
+});
+
+const hero_left = document.querySelector(".hero-main_left");
